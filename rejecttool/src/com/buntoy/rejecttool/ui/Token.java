7773ab7ddf9a777960b98e055bba71f09732c36f
eth2cancel <42766812+eth2cancel@users.noreply.github.com>
2018-08-28 16:49:28 +0800

    Update Token.java

--- rejecttool/src/com/buntoy/rejecttool/ui/Token.java
+++ rejecttool/src/com/buntoy/rejecttool/ui/Token.java
@@ -1,1 +1,27 @@
+package com.eth2cancel.rejecttool.ui;
 
+import javax.swing.JFrame;
+
+public class Token {
+	
+	
+	
+	public static void main(String[] args) {
+	/*	
+		
+		
+		String string ="111|222|3333|444";
+		String[] array = string.split("\\|");
+		for (String string2 : array) {
+			System.out.println(string2);
+			
+		}*/
+	
+		//JFrame frame =e.a();
+		JFrame frame = UiFrame.placeComponents();
+        frame.setVisible(true);
+	
+	}
+	
+	
+}
